7173b8b9020c73783813fced5141ac67b51ef394
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-12-20 19:28:08 +0300

    task 4 - create yandex_vpc_network and yandex_vpc_subnet using terraform

--- Task4/terraform.tfvars
+++ Task4/terraform.tfvars
@@ -3,6 +3,8 @@ yc_cloud_id  = "b1geu755mpn0frenm65h"
 yc_folder_id = "b1g8e80hvfhhkrvujj98"
 yc_zone      = "ru-central1-a"
 
-subnet_id = "e9bnia4jv4itu3gad6b8"
+ssh_public_key_path = "./.ssh/id_rsa.pub"
 
-ssh_public_key_path = "C:/Users/autia/.ssh/id_rsa.pub"
+vm_cores = 2
+vm_memory = 2
+vm_disk_size = 20
--- Task4/variables.tf
+++ Task4/variables.tf
@@ -19,13 +19,28 @@ variable "yc_zone" {
   default     = "ru-central1-a"
 }
 
-variable "subnet_id" {
-  description = "ID существующей подсети"
-  type        = string
+variable "cidr_blocks" {
+  type    = list(string)
+  default = ["10.10.0.0/24"]
 }
 
 variable "ssh_public_key_path" {
   description = "Путь к публичному SSH-ключу"
   type        = string
   default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "vm_cores" {
+  description = "Количество ядер в VM"
+  type = number
+}
+
+variable "vm_memory" {
+  description = "Размер RAM в VM в ГБ"
+  type = number
+}
+
+variable "vm_disk_size" {
+  description = "Размер диска в VM в ГБ"
+  type = number
 }
